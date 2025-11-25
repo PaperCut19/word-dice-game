6144a0d2bd82c1ffb5efc553f7ad23a187b575fb
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-11-25 02:34:41 -0500

    modify how dice display -move the code that controls dice displaying on manage-dice.html into its own javascript file -for the dice on index.html, create an array of letters and display a dice for each letter

--- src/main.js
+++ src/main.js
@@ -1,36 +1,25 @@
 import "./style.css";
-import { Dice } from "./diceLogic";
 
-const dice1 = new Dice(1);
-const dice2 = new Dice(2);
+const frontPageDice = ["W", "O", "R", "D", "S"];
 
-const diceArray = [];
-diceArray.push(dice1, dice2);
+const frontPageDiceDiv = document.querySelector("#frontPageDice");
 
-const faceValueButton = document.querySelector("#faceValueButton");
-const diceBlockDiv = document.querySelector("#diceBlockDiv");
-
-faceValueButton.addEventListener("click", () => {
-  dice1.showFaces();
-});
-
-diceArray.forEach((dice) => {
+frontPageDice.forEach((dice) => {
   const diceElement = document.createElement("div");
 
   diceElement.innerHTML = `
-  <div class="group flex flex-col items-center">
-      <h1>Dice ID: ${dice.id}</h1>
+  <div class="group">
           <div
             class="flex h-40 w-40 items-center justify-center rounded-4xl bg-gray-300 duration-400 group-hover:cursor-pointer group-hover:bg-gray-200"
           >
             <h1
               class="text-8xl text-red-500 duration-400 group-hover:text-red-400"
             >
-              ${dice.mainFace}
+              ${dice}
             </h1>
           </div>
         </div>
   `;
 
-  diceBlockDiv.appendChild(diceElement);
+  frontPageDiceDiv.appendChild(diceElement);
 });
