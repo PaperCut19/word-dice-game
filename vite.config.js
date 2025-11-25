967ad7526037acbf92234f7759170ec817b50352
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-11-25 00:37:22 -0500

    fix deployment problem -tell vite to also build manage-dice.html and put it in dist folder because if we don't, the online server will never see the html file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        manage: resolve(__dirname, "manage-dice.html"),
+      },
+    },
+  },
 });
