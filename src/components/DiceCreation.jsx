b429a4c53414d2398fc8b6c0fef88c07d381c2cc
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-11-29 18:29:46 -0500

    fix positioning of the header in DiceCreation component -use text-center on it

--- src/components/DiceCreation.jsx
+++ src/components/DiceCreation.jsx
@@ -39,84 +39,87 @@ function DiceCreation({ onSubmit, diceToEdit = null }) {
   };
 
   return (
-    <div className="border-main flex flex-col gap-2 md:flex-row items-center">
+    <div className="border-main">
       {/* Header - only shows when editing */}
       {diceToEdit && (
-        <h2 className="text-2xl font-bold">Editing: {diceToEdit.name}</h2>
+        <h2 className="text-2xl font-bold text-center mb-5">
+          Editing: {diceToEdit.name}
+        </h2>
       )}
+      <div className=" flex flex-col gap-2 md:flex-row items-center">
+        {/* dice preview */}
+        <DiceDataBlock dice={{ name: diceName, text1: text1 }} />
 
-      {/* dice preview */}
-      <DiceDataBlock dice={{ name: diceName, text1: text1 }} />
+        {/* form inputs */}
+        <div className="flex flex-col gap-2 md:ml-5">
+          <label htmlFor="nameOfNewDiceText">Name of Dice</label>
+          <input
+            type="text"
+            id="nameOfNewDiceText"
+            className="text-input"
+            value={diceName}
+            onChange={(event) => setDiceName(event.target.value)}
+          />
 
-      {/* form inputs */}
-      <div className="flex flex-col gap-2 md:ml-5">
-        <label htmlFor="nameOfNewDiceText">Name of Dice</label>
-        <input
-          type="text"
-          id="nameOfNewDiceText"
-          className="text-input"
-          value={diceName}
-          onChange={(event) => setDiceName(event.target.value)}
-        />
+          <label htmlFor="createDiceText1">Side 1</label>
+          <input
+            type="text"
+            id="createDiceText1"
+            className="text-input"
+            value={text1}
+            onChange={(event) => setText1(event.target.value[0] || "")}
+            maxLength={1}
+          />
+          <label htmlFor="createDiceText2">Side 2</label>
+          <input
+            type="text"
+            id="createDiceText2"
+            className="text-input"
+            value={text2}
+            onChange={(event) => setText2(event.target.value[0] || "")}
+            maxLength={1}
+          />
+          <label htmlFor="createDiceText3">Side 3</label>
+          <input
+            type="text"
+            id="createDiceText3"
+            className="text-input"
+            value={text3}
+            onChange={(event) => setText3(event.target.value[0] || "")}
+            maxLength={1}
+          />
+          <label htmlFor="createDiceText4">Side 4</label>
+          <input
+            type="text"
+            id="createDiceText4"
+            className="text-input"
+            value={text4}
+            onChange={(event) => setText4(event.target.value[0] || "")}
+            maxLength={1}
+          />
+          <label htmlFor="createDiceText5">Side 5</label>
+          <input
+            type="text"
+            id="createDiceText5"
+            className="text-input"
+            value={text5}
+            onChange={(event) => setText5(event.target.value[0] || "")}
+            maxLength={1}
+          />
+          <label htmlFor="createDiceText6">Side 6</label>
+          <input
+            type="text"
+            id="createDiceText6"
+            className="text-input"
+            value={text6}
+            onChange={(event) => setText6(event.target.value[0] || "")}
+            maxLength={1}
+          />
 
-        <label htmlFor="createDiceText1">Side 1</label>
-        <input
-          type="text"
-          id="createDiceText1"
-          className="text-input"
-          value={text1}
-          onChange={(event) => setText1(event.target.value[0] || "")}
-          maxLength={1}
-        />
-        <label htmlFor="createDiceText2">Side 2</label>
-        <input
-          type="text"
-          id="createDiceText2"
-          className="text-input"
-          value={text2}
-          onChange={(event) => setText2(event.target.value[0] || "")}
-          maxLength={1}
-        />
-        <label htmlFor="createDiceText3">Side 3</label>
-        <input
-          type="text"
-          id="createDiceText3"
-          className="text-input"
-          value={text3}
-          onChange={(event) => setText3(event.target.value[0] || "")}
-          maxLength={1}
-        />
-        <label htmlFor="createDiceText4">Side 4</label>
-        <input
-          type="text"
-          id="createDiceText4"
-          className="text-input"
-          value={text4}
-          onChange={(event) => setText4(event.target.value[0] || "")}
-          maxLength={1}
-        />
-        <label htmlFor="createDiceText5">Side 5</label>
-        <input
-          type="text"
-          id="createDiceText5"
-          className="text-input"
-          value={text5}
-          onChange={(event) => setText5(event.target.value[0] || "")}
-          maxLength={1}
-        />
-        <label htmlFor="createDiceText6">Side 6</label>
-        <input
-          type="text"
-          id="createDiceText6"
-          className="text-input"
-          value={text6}
-          onChange={(event) => setText6(event.target.value[0] || "")}
-          maxLength={1}
-        />
-
-        <Button onClick={handleSubmit}>
-          {diceToEdit ? "Update Dice" : "Create Dice"}
-        </Button>
+          <Button onClick={handleSubmit}>
+            {diceToEdit ? "Update Dice" : "Create Dice"}
+          </Button>
+        </div>
       </div>
     </div>
   );
