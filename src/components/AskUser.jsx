2dadd5a47c154e87e4b994a139bab2ebda10880b
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-11-29 19:13:55 -0500

    improve user experience when submitting things -use onKeyDown on the input field so that if the user presses Enter on their keyboard, it does the same thing as clicking on the submit button

--- src/components/AskUser.jsx
+++ src/components/AskUser.jsx
@@ -19,6 +19,7 @@ function AskUser({ label, placeholder, onSubmit, buttonText = "Submit" }) {
         placeholder={placeholder}
         value={inputValue}
         onChange={(event) => setInputValue(event.target.value)}
+        onKeyDown={(event) => event.key === "Enter" && handleSubmit()}
       />
       <Button onClick={handleSubmit}>{buttonText}</Button>
     </div>
