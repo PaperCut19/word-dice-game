be8334ddb2f42e7f3cce12aeb163d98f683440d9
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-11-30 00:54:04 -0500

    build sections in PlayArea component -create a left and right section, on mobile they'll stack vertically

--- src/PlayArea.jsx
+++ src/PlayArea.jsx
@@ -3,11 +3,23 @@ import Button from "./components/Button";
 function PlayArea({ setCurrentPage }) {
   return (
     <div className="main-container">
-      <h1 className="font-fancyLetters text-4xl">Play Area</h1>
+      <h1 className="mb-8 font-fancyLetters text-4xl">Play Area</h1>
 
-      <p>Play area content will go here</p>
+      {/* two sections side by side on desktop */}
+      <div className="flex flex-col gap-4 md:flex-row">
+        {/* left section, dice selector */}
+        <div className="border-main">
+          <h2 className="text-2xl font-bold">Your Dice</h2>
+          <p>Dice selector will go here</p>
+        </div>
 
-      {/* Back to home button */}
+        {/* right section, play area */}
+        <div className="border-main">
+          <h2 className="text-2xl font-bold">Play Area</h2>
+          <p>Active dice will go here</p>
+        </div>
+      </div>
+
       <Button className="button-primary" onClick={() => setCurrentPage("home")}>
         Home Page
       </Button>
