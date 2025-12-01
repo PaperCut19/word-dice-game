ed685d7540475d18808e3fa313c5f7f1f24d5316
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-12-01 00:42:51 -0500

    fix cursor hover effect bug -change the group-hover stuff to just hover and get rid of the group thing, as long as the user hovers over the dice-box it should be fine

--- src/components/DiceDataBlock.jsx
+++ src/components/DiceDataBlock.jsx
@@ -10,22 +10,22 @@ function DiceDataBlock({ dice, showMetadata = true }) {
 
     // If empty, return default/gray
     if (!letter) {
-      return "bg-gray-300 group-hover:bg-gray-400";
+      return "bg-gray-300 hover:bg-gray-400";
     }
 
     // Otherwise, auto-determine from letter
     const vowels = ["A", "E", "I", "O", "U"];
     return vowels.includes(letter)
-      ? "bg-red-400 group-hover:bg-red-300"
-      : "bg-blue-400 group-hover:bg-blue-300";
+      ? "bg-red-400 hover:bg-red-300"
+      : "bg-blue-400 hover:bg-blue-300";
   };
 
   return (
-    <div className="group flex flex-col items-center" onClick={dice.onClick}>
+    <div className="flex flex-col items-center" onClick={dice.onClick}>
       {showMetadata && (
         <>
           <h1>Dice Name: {dice.name || "N/A"}</h1>
-          <h1>Dice ID: {dice.id || ""}</h1>
+          <h1 className="text-sm">Dice ID: {dice.id || ""}</h1>
           {dice.frozen && (
             <h1 className="pb-2 text-xl text-blue-400 underline underline-offset-4">
               Frozen
