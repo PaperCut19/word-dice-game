d7dcd9e640b12fbbd1eb31aa4429f1fc409c4531
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-12-02 23:39:55 -0500

    modify login component so that it can be more reusable -remove all the divs and styling that was turning login component into a full page because that's not what it's supposed to be

--- src/Authentication.jsx
+++ src/Authentication.jsx
@@ -1,15 +1,26 @@
 import Login from "./components/Login";
+import { useState } from "react";
+import Button from "./components/Button";
 
 function Authentication() {
+  const [userMode, setUserMode] = useState("login");
+
   return (
-    <>
-      <Login
-        usernameMessage={"Enter Username"}
-        passwordMessage={"Enter Password"}
-        submitMessage={"Login"}
-        authenticationTitle={"Login"}
-      />
-    </>
+    <div className="main-container">
+      <h1 className="font-fancyLetters text-4xl">Login</h1>
+      {userMode === "login" && (
+        <>
+          <div className="border-main flex flex-col justify-center gap-3">
+            <Login
+              usernameMessage={"Enter Username"}
+              passwordMessage={"Enter Password"}
+              submitMessage={"Login"}
+              authenticationTitle={"Login"}
+            />
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,31 @@
 import { useState } from "react";
 
-function Login({
-  usernameMessage,
-  passwordMessage,
-  submitMessage,
-  authenticationTitle,
-}) {
+function Login({ usernameMessage, passwordMessage, submitMessage }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   return (
-    <div className="main-container">
-      <h1 className="font-fancyLetters text-4xl">{authenticationTitle}</h1>
-
-      <div className="border-main flex flex-col justify-center">
-        <div className="flex flex-col gap-3">
-          <input
-            type="text"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-            placeholder={usernameMessage}
-            className="text-input w-50 text-center"
-          />
-          <input
-            type="password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            placeholder={passwordMessage}
-            className="text-input w-50 text-center"
-          />
-          <p className="text-red-500">{errorMessage}</p>
-        </div>
-        <button className="button-primary">{submitMessage}</button>
+    <>
+      <div className="flex flex-col gap-3">
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          placeholder={usernameMessage}
+          className="text-input w-50 text-center"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          placeholder={passwordMessage}
+          className="text-input w-50 text-center"
+        />
+        <p className="text-red-500">{errorMessage}</p>
       </div>
-    </div>
+      <button className="button-primary">{submitMessage}</button>
+    </>
   );
 }
 
