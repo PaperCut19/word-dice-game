1df89bc611ce1750ba2b66e3dff692dc89f91539
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-12-02 23:53:30 -0500

    modify Authentication.jsx -create a user interface that allows you to either login or sign up

--- src/Authentication.jsx
+++ src/Authentication.jsx
@@ -3,11 +3,29 @@ import { useState } from "react";
 import Button from "./components/Button";
 
 function Authentication() {
-  const [userMode, setUserMode] = useState("login");
+  const [userMode, setUserMode] = useState("none");
+
+  let headerMessage = "";
+
+  if (userMode === "none") {
+    headerMessage = "Login or Sign Up";
+  } else if (userMode === "login") {
+    headerMessage = "Login";
+  } else if (userMode === "signup") {
+    headerMessage = "Sign Up";
+  }
 
   return (
     <div className="main-container">
-      <h1 className="font-fancyLetters text-4xl">Login</h1>
+      <h1 className="mb-5 font-fancyLetters text-4xl">{headerMessage}</h1>
+
+      {userMode === "none" && (
+        <div className="border-main flex w-50 flex-col justify-center gap-2">
+          <Button className="button-primary w-full">Login</Button>
+          <Button className="button-secondary w-full">Sign Up</Button>
+        </div>
+      )}
+
       {userMode === "login" && (
         <>
           <div className="border-main flex flex-col justify-center gap-3">
