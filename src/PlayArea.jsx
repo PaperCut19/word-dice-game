9eddbf1297c88ab68cbffb13b67912c2d26c7116
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-12-08 18:00:41 -0500

    Fix text wrapping and improve vertical alignment
    Removes 'w-max' from the Play Area grid to allow dice names to wrap properly. Adds 'items-end' to both grids so dice align to the bottom of the row, creating a better 'sitting on table' visual effect.

--- src/PlayArea.jsx
+++ src/PlayArea.jsx
@@ -119,7 +119,7 @@ function PlayArea({ setCurrentPage, currentPage }) {
         <div className="border-main min-w-0">
           <h2 className="yellow-underlined-heading">Your Dice</h2>
 
-          <div className="grid h-fit grid-flow-col grid-rows-[auto_auto] gap-3 overflow-x-auto">
+          <div className="grid h-fit grid-flow-col grid-rows-[auto_auto] items-end gap-3 overflow-x-auto">
             {loading ? (
               <div className="p-4 text-gray-500 italic">
                 Loading collection...
@@ -144,7 +144,7 @@ function PlayArea({ setCurrentPage, currentPage }) {
 
           {/* display active dice */}
           <div className="w-full overflow-x-auto">
-            <div className="mx-auto mb-4 grid h-fit w-max auto-cols-auto grid-flow-col grid-rows-[auto_auto] gap-3 pb-2">
+            <div className="mx-auto mb-4 grid h-fit auto-cols-auto grid-flow-col grid-rows-[auto_auto] items-end gap-3 pb-2">
               {displayDiceArray.length > 0 ? (
                 displayDiceArray.map((dice) => (
                   <DiceDataBlock key={dice.playId} dice={dice} />
